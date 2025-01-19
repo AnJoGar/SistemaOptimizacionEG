f6bb203fe32623bda14105731ef8b46b60fd7b5f
AnJoGar <andjor2020@gmail.com>
2025-01-19 01:28:00 -0500

    avance

--- Backend/Sistema_optimizacion_PG/formularios/views.py
+++ Backend/Sistema_optimizacion_PG/formularios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.exceptions import NotFound
 # Create your views here.
 # forms.py
 
@@ -36,6 +36,25 @@ class SolicitudVisaAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Método GET para obtener todas las solicitudes de visa
+    def get(self, request):
+        solicitudes = SolicitudVisa.objects.all()
+        serializer = SolicitudVisaSerializer(solicitudes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, *args, **kwargs):
+        # Buscar la solicitud por el id que viene en el JSON
+        id = request.data.get('id')
+        try:
+            solicitud = SolicitudVisa.objects.get(id=id)
+        except SolicitudVisa.DoesNotExist:
+            return Response({'detail': 'Solicitud no encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        # Actualizar los campos de la solicitud con los datos del JSON
+        serializer = SolicitudVisaSerializer(solicitud, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
 class SolicitudPasaporteAPIView(APIView):
     def post(self, request):
@@ -44,6 +63,11 @@ class SolicitudPasaporteAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Método GET para obtener todas las solicitudes de pasaporte
+    def get(self, request):
+        solicitudes = SolicitudPasaporte.objects.all()
+        serializer = SolicitudPasaporteSerializer(solicitudes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class SolicitudCedulaAPIView(APIView):
     def post(self, request):
@@ -52,3 +76,8 @@ class SolicitudCedulaAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Método GET para obtener todas las solicitudes de cédula
+    def get(self, request):
+        solicitudes = SolicitudCedula.objects.all()
+        serializer = SolicitudCedulaSerializer(solicitudes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
